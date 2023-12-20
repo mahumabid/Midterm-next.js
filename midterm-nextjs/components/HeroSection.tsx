14a2ea14a7edbeccbe4fbb0db9321e3b16fc9949
Mahum Abid <mahumabid101@gmail.com>
2023-12-20 13:22:59 -0500

    closes branch styled hero section and text

--- midterm-nextjs/components/HeroSection.tsx
+++ midterm-nextjs/components/HeroSection.tsx
@@ -1,20 +1,36 @@
-import React from 'react'
+"use client" // this is a client component
+import React from "react"
+import Image from "next/image"
+import { Link } from "react-scroll/modules"
+import { HiArrowDown } from "react-icons/hi"
 
 const HeroSection = () => {
   return (
     <section id="home">
-      <div>
-        <div>
-          <img src="/headshot.jpg" alt="" width={300} height={300} />
-          {/* The 'img' tag should have 'src' attribute, not 'source' */}
-          <div></div>
+      <div className="flex flex-col text-center items-center justify-center animate-fadeIn animation-delay-2 my-10 py-16 sm:py-32 md:py-48 md:flex-row md:space-x-4 md:text-left">
+        <div className="md:mt-2 md:w-1/2">
+          <Image
+            src="/headshot.png"
+            alt=""
+            width={325}
+            height={325}
+            className="rounded-full shadow-2xl"
+          />
+        </div>
+        <div className="md:mt-2 md:w-3/5">
+          <h1 className="text-4xl font-bold mt-6 md:mt-0 md:text-7xl">Hi, I&#39;m Mahum!</h1>
+          <p className="text-lg mt-4 mb-6 md:text-2xl">
+            I&#39;m a{" "}
+            <span className="font-semibold text-black-600">
+              Data Analyst{" "}
+            </span>
+            based in Jersey City, NJ. Working towards analyzing data that
+            makes data bring insights and ways to solve problems
+          </p>
         </div>
       </div>
     </section>
-  );
+  )
 }
 
-export default HeroSection;
-
-
-
+export default HeroSection
