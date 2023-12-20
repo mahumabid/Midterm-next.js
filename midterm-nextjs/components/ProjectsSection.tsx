07c9d687c88bf891e1b1ccd91dc76eea20233e22
Mahum Abid <mahumabid101@gmail.com>
2023-12-20 16:21:46 -0500

    closes branch created nested div

--- midterm-nextjs/components/ProjectsSection.tsx
+++ midterm-nextjs/components/ProjectsSection.tsx
@@ -5,41 +5,63 @@ import { BsGithub, BsArrowUpRightSquare } from "react-icons/bs"
 
 const projects = [
     {
-      name: "Thankful Thoughts",
+      name: "Price Prediction ML",
       description:
-        "ThankfulThoughts is a web app that generates an appreciative sentence of something or someone you are thankful for.",
-      image: "/thankfulthoughts.png",
+        "Streamlit app SuperDuper which offers users the chance to dupe their favorite high-end products and find cheaper alternatives that are at the ingredient-level similar..",
+      image: "/a.png",
     },
     {
-      name: "PlatoIO",
-      description: "PlatoIO is a to do list app that built using the PERN stack.",
-      image: "/platoio.png",
+      name: "Microsoft's VS",
+      description: "Generated a Recommendation System with Hybrid Approach for Improved Accuracy in Predicting User Preferences in Video Content for Microsoft.",
+      image: "/b.png",
     },
     {
-      name: "Kator Family Photos",
+      name: "H1N1 2009 Vaccination Campaign",
       description:
-        "Kator Family Photos is a photos and video digitization service in the LA area.",
-      image: "/familyphotos.png",
+        "Built a model that can use the patient demographics data, collected following the 2009 H1N1 virus outbreak to predict which patients are likely to get the vaccine.",
+      image: "/c.png",
     },
   ]
   
-const ProjectsSection = () => {
-  return (
-    <section id="projects">
-         <h1 className="text-center font-bold text-4xl">
-            Projects
-            <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
-          </h1>
-
-            <div className="flex flex-col space-y-28"></div>
-            {projects.map((project, idx)=>{
-                return <div key={idx}>
-                    
-
-
-                </div>})}
-    </section >
-  )
-}
-
-export default ProjectsSection
+  const ProjectsSection = () => {
+    return (
+      <section id="projects">
+        <h1 className="my-10 text-center font-bold text-4xl">
+          Projects
+          <hr className="w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded"></hr>
+        </h1>
+  
+        <div className="flex flex-col space-y-28">
+          {projects.map((project, idx) => {
+            return (
+              <div key={idx}>
+                  <div className="flex flex-col  animate-slideUpCubiBezier animation-delay-2 md:flex-row md:space-x-12">
+                    <div className=" md:w-1/2">
+                        <Image
+                          src={project.image}
+                          alt=""
+                          width={1000}
+                          height={1000}
+                          className="rounded-xl shadow-xl hover:opacity-70"
+                        />
+                    </div>
+                    <div className="mt-8 md:w-1/2">
+                      <h1 className="text-4xl font-bold mb-6">{project.name}</h1>
+                      <p className="text-xl leading-7 mb-4 text-neutral-600 dark:text-neutral-400">
+                        {project.description}
+                      </p>
+                      <div className="flex flex-row align-bottom space-x-4">
+                      </div>
+                    </div>
+                  </div>
+              </div>
+            )
+          })}
+          
+        </div>
+      </section>
+    )
+  }
+  
+  export default ProjectsSection
+  
