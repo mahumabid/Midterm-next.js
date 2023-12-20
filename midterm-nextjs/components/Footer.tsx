08c1646df7f46073e033d80e3ea799d3e8f59336
Mahum Abid <mahumabid101@gmail.com>
2023-12-20 17:37:48 -0500

    closes branch border and padding added

--- midterm-nextjs/components/Footer.tsx
+++ midterm-nextjs/components/Footer.tsx
@@ -9,7 +9,10 @@ import {
 
 const Footer = () => {
   return (
-    <footer></footer>
+    <footer className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl ">
+        <hr className="w-full h-0.5 mx-auto mt-8 bg-neutral-200 border-0"></hr>
+
+    </footer>
   )
 }
 
