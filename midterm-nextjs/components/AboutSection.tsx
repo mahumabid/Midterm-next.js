7c2166fb16491ed72f1aca2c3247c47189742994
Mahum Abid <mahumabid101@gmail.com>
2023-12-20 14:08:16 -0500

    closes branch about file with basic body has been coded

---
+++ midterm-nextjs/components/AboutSection.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const AboutSection = () => {
+  return (
+    <section id="about">
+        <div>
+            <h1>About Me</h1>
+            <div>
+                <h1></h1>
+                <p></p>
+            </div>
+            <div>
+                <h1></h1>
+                <p></p>
+            </div>
+
+        </div>
+    </section>
+  )
+}
+
+export default AboutSection
