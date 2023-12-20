a9c9a5a1c723f805fc8c623a13c208c8a55ecf53
Mahum Abid <mahumabid101@gmail.com>
2023-12-19 22:29:26 -0500

    closes branch as nav items have been assigned a type

--- midterm-nextjs/components/Navbar.tsx
+++ midterm-nextjs/components/Navbar.tsx
@@ -7,6 +7,26 @@ import { useTheme } from "next-themes"
 import { RiMoonFill, RiSunLine } from "react-icons/ri"
 import { IoMdMenu, IoMdClose } from "react-icons/io"
 
+interface NavItem {
+  label: string
+  page: string
+}
+
+const NAV_ITEMS: Array<NavItem> = [
+  {
+    label: "Home",
+    page: "home",
+  },
+  {
+    label: "About",
+    page: "about",
+  },
+  {
+    label: "Projects",
+    page: "projects",
+  },
+]
+
 const Navbar = () => {
   return (
     <div>Navbar</div>
