bcdf0f95834d1cb4734ba69edb2f63dcc224d673
Mahum Abid <mahumabid101@gmail.com>
2023-12-20 15:13:39 -0500

    closes branch as basic body or projects is added

--- midterm-nextjs/app/page.tsx
+++ midterm-nextjs/app/page.tsx
@@ -1,5 +1,6 @@
 import HeroSection from "../components/HeroSection"
 import AboutSection from "../components/AboutSection"
+import ProjectsSection from "../components/ProjectsSection"
 
 export default function Home() {
     return (
