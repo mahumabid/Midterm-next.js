f7b769555d904f78a28700e84f21ff58e36dec10
Mahum Abid <mahumabid101@gmail.com>
2023-12-19 22:06:51 -0500

    closes branch all required modules are imported

--- midterm-nextjs/components/Navbar.tsx
+++ midterm-nextjs/components/Navbar.tsx
@@ -1,4 +1,11 @@
+"use client" // this is a client component
 import React from "react"
+import { useState } from "react"
+import { Link } from "react-scroll/modules"
+import { usePathname } from "next/navigation"
+import { useTheme } from "next-themes"
+import { RiMoonFill, RiSunLine } from "react-icons/ri"
+import { IoMdMenu, IoMdClose } from "react-icons/io"
 
 const Navbar = () => {
     return(
